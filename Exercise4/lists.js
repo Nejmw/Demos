4cd9fd99f65848ef0078d725b59c6ce2b64c284e
nejmw <nejmw@hotmail.com>
2022-10-21 16:29:48 +0100

    Added Sky demo exercises

---
+++ Exercise4/lists.js
@@ -0,0 +1,14 @@
+'use strict'
+
+var x = 2;
+
+if(x>3){
+    console.log("x is greater than 3");
+} else if(x==3){
+    console.log("x is equal to 3");
+} else {
+    console.log("x is not greater than 3");
+}
+function myNewFunction(){
+    document.getElementById("football").innerHTML='European Champions';
+}
